c1e930b3a740e491c363c74fc04595a102ef196a
JackieTreeh0rn <1523839+JackieTreeh0rn@users.noreply.github.com>
2024-10-02 03:14:24 -0400

    update remove script

--- remove.sh
+++ remove.sh
@@ -4,3 +4,7 @@
 docker stack rm pihole
 docker network rm pihole
 
+sudo rm -rvf /mnt/glusterfs/pihole/pihole-dnsmasq/*
+sudo rm -rvf /mnt/glusterfs/pihole/pihole-data/*
+sudo rm -rvf /mnt/glusterfs/pihole/lighttpd/*
+
