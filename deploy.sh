522218d81481991b17715fbca2a787b10938f4aa
JackieTreeh0rn <1523839+JackieTreeh0rn@users.noreply.github.com>
2024-11-11 04:02:57 -0500

    deployment edits

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-docker pull pihole/pihole:2024.07.0
+set -o allexport
+. ./pihole-orbital.env
 
+docker pull pihole/pihole:latest
+
+# Deploy custom network - edit to reflect desired network or comment out #
 docker network create -d overlay --subnet=10.0.53.0/24 --gateway=10.0.53.1 pihole
 
+# Deploy PiHole
 docker stack deploy -c docker-compose-stack-hostmode.yml pihole
--- remove.sh
+++ remove.sh
@@ -2,9 +2,5 @@
 
 
 docker stack rm pihole
-docker network rm pihole
-
-sudo rm -rvf /mnt/glusterfs/pihole/pihole-dnsmasq/*
-sudo rm -rvf /mnt/glusterfs/pihole/pihole-data/*
-sudo rm -rvf /mnt/glusterfs/pihole/lighttpd/*
+# docker network rm pihole
 
